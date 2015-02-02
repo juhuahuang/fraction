26af0676df97d419574e4c68bde5a551d8fc2347
juhuahuang <hjh.1222@gmail.com>
2015-02-02 11:04:43 -0500

    finish all member functions

--- fraction.cpp
+++ fraction.cpp
@@ -1,35 +1,43 @@
-//fraction.cpp
-#include <iostream>
+#include <iostream> 
+#include <cassert> 
+#include <cmath>  
 #include "fraction.h"
-
-using namesapce std;
-
+using namespace std;
 template<class T>
-fraction<T>::fraction(T numer, T denom):numerator((abs(num)),denominator(abs(denom)){
-	assert(denominator!=0);
+fraction<T>::fraction(T numer, T denom){
+	// denominator can't be 0
+	assert(denom!=0);
+	numerator = numer<0? -numer:numer;
+	denominator = denom<0 ? -denom:denom;
 	whole_number = 0;
 	negative =  (numer!=0) && (numer>0) ^ (denom>0) ;
 }
 
 template<class T>
-fraction<T>::fraction(T numer, T denom, int whole):numerator((abs(num)),denominator(abs(denom)),whole_number(abs(whole)){
-	assert(denominator!=0 && whole >= 0 );
+fraction<T>::fraction(T numer, T denom, int whole){
+	// denominator can't be 0 and whole_number has to be positive or 0;
+	assert(denom!=0 && whole >= 0 );
+	numerator = numer<0? -numer:numer;
+	denominator = denom<0 ? -denom:denom;
+	whole_number = whole;
 	negative =  (numer>0) ^ (denom>0);
 }
 
-
-
+//construct fraction from a single number.
 template<class T>
 fraction<T>::fraction(T number){
 	negative = number<0;
-	whole_number = (int)number - 0;
-	T remains = number - whole_number;
+	number = abs(number);
+	whole_number = (int)number;
+	T remains = (T) (number - whole_number);
+	cout<<remains<<endl;
 	numerator = 0;
 	denominator = 1;
 	while( remains > 0){
-		numerator = numerator*10 + (int)remain*10; 
+		cout<<remains<<endl;
+		numerator = numerator*10 + round (remains*10); 
 		denominator *=10;
-		remains = remains *10 - (int)remains*10;  
+		remains =remains *10 - round (remains*10);
 	}
 }
 
@@ -38,49 +46,203 @@ template<class T>
 fraction<T> fraction<T> ::operator+(const fraction &X) const{
 	T new_numerator;
 	T new_denominator;
-	int new_whole_number;
 	
-	new_numerator = (negative ? -numerator:numerator) * X.denominator + (X.negative ? -X.numerator:X.numerator)*denominator; 
+	T numerator1 = numerator + whole_number*denominator;
+	T numerator2 = X.numerator + X.whole_number * X.denominator;
+	
+	new_numerator = (negative ? -numerator1:numerator1) * X.denominator + (X.negative ? -numerator2:numerator2)*denominator; 
 	new_denominator = denominator * X.denominator;
-	new_whole_number = negative ? -whole_number:whole_number + X.negative ? -X.whole_number:X.whole_number;
 	
-	return fraction(new_numerator,new_denominator,new_whole_number);
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if(whole_number != 0 && X.whole_number != 0){
+		bool new_negative = new_numerator<0;
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		int new_whole_number = (int)new_numerator /new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		return fraction(new_numerator,new_denominator, new_whole_number);
+	}
+	else return fraction(new_numerator,new_denominator);
 	} 
+
+// for fraction<int>, fraction reduction will be done.
+template<>
+fraction<int> fraction<int> ::operator+(const fraction &X) const{
+	int new_numerator;
+	int new_denominator;
+	
+	int numerator1 = numerator + whole_number*denominator;
+	int numerator2 = X.numerator + X.whole_number * X.denominator;
+	
+	new_numerator = (negative ? -numerator1:numerator1) * X.denominator + (X.negative ? -numerator2:numerator2)*denominator; 
+	new_denominator = denominator * X.denominator;
+	
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if(whole_number != 0 && X.whole_number != 0){
+		bool new_negative = new_numerator<0;
+		new_numerator = abs(new_numerator);
+		int new_whole_number = (int)new_numerator /new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		return fraction(new_numerator/greatest_common_divisor,new_denominator/greatest_common_divisor, new_whole_number);
+	}
+	else {
+		// fraction reduction
+		int greatest_common_divisor = GCD(abs(new_numerator),new_denominator);
+		return fraction(new_numerator/greatest_common_divisor, new_denominator/greatest_common_divisor);
+
+	} 
+}
+
 
 template<class T>
 fraction<T> fraction<T> ::operator-(const fraction &X) const{
 	T new_numerator;
 	T new_denominator;
-	int new_whole_number;
-	new_numerator = (negative ? -numerator:numerator) * X.denominator - (X.negative ? -X.numerator:X.numerator) * denominator; 
-	new_denominator = denominator * X.denominator;
-	new_whole_number = (negative ? -whole_number:whole_number) - (X.negative ? -X.whole_number:X.whole_number);
 
-	return fraction(new_numerator,new_denominator,new_whole_number);
-	} 
+	T numerator1 = numerator + whole_number*denominator;
+	T numerator2 = X.numerator + X.whole_number * X.denominator;
 	
+	new_numerator = (negative ? -numerator1:numerator1) * X.denominator - (X.negative ? -numerator2:numerator2)*denominator; 
+	new_denominator = denominator * X.denominator;
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if(whole_number != 0 && X.whole_number != 0){
+		bool new_negative = new_numerator<0;
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		int new_whole_number = (int)new_numerator /new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		return fraction(new_numerator,new_denominator, new_whole_number);
+	}
+	else return fraction(new_numerator,new_denominator);
+
+	} 
+
+
+// for fraction<int>, fraction reduction will be done.
+template<>
+fraction<int> fraction<int> ::operator-(const fraction &X) const{
+	int new_numerator;
+	int new_denominator;
+
+	int numerator1 = numerator + whole_number*denominator;
+	int numerator2 = X.numerator + X.whole_number * X.denominator;
+	
+	new_numerator = (negative ? -numerator1:numerator1) * X.denominator - (X.negative ? -numerator2:numerator2)*denominator; 
+	new_denominator = denominator * X.denominator;
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if(whole_number != 0 && X.whole_number != 0){
+		bool new_negative = new_numerator<0;
+		new_numerator = abs(new_numerator);
+		int new_whole_number = (int)new_numerator /new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = new_negative ? -new_numerator:new_numerator;
+		return fraction(new_numerator/greatest_common_divisor,new_denominator/greatest_common_divisor, new_whole_number);
+	}
+	else {
+		// fraction reduction
+		int greatest_common_divisor = GCD(abs(new_numerator),new_denominator);
+		return fraction(new_numerator/greatest_common_divisor, new_denominator/greatest_common_divisor);
+
+	} 
+}	
+
 template<class T>	
 fraction<T> fraction<T> :: operator*(const fraction &X) const{
 	T new_numerator = (numerator + whole_number*denominator)  * (X.numerator + X.whole_number*X.denominator);
 	T new_denominator = denominator * X.denominator;
-	int new_whole_number = (int)new_numbertor / new_denominator;
-	new_numbertor = new_numbertor -  new_whole_number;
-	new_numerator = negative^X.negative ? -new_numerator : numerator;
-	return fraction(new_numerator ,new_denominator, new_whole_number);
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if( whole_number != 0 && X.whole_number != 0){
+		int new_whole_number = (int)new_numerator / new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator ,new_denominator, new_whole_number);
+	}
+
+	else{
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator ,new_denominator);
+	}
+}
+
+
+// for fraction<int>, fraction reduction will be done.
+template<>	
+fraction<int> fraction<int> :: operator*(const fraction &X) const{
+	int new_numerator = (numerator + whole_number*denominator)  * (X.numerator + X.whole_number*X.denominator);
+	int new_denominator = denominator * X.denominator;
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if( whole_number != 0 && X.whole_number != 0){
+		int new_whole_number = (int)new_numerator / new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator/greatest_common_divisor ,new_denominator/greatest_common_divisor, new_whole_number);
+	}
+
+	else{	
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator /greatest_common_divisor ,new_denominator/greatest_common_divisor);
+	}
 }
 
 
 template<class T>
-fraction<T> fraction<T> :: operator\(const fraction &X) const{
+fraction<T> fraction<T> :: operator/(const fraction &X) const{
 	T new_numerator = (numerator + whole_number*denominator) * X.denominator;
 	T new_denominator = denominator * (X.numerator + X.whole_number*X.denominator);
 	assert(new_denominator!=0);
-	int new_whole_number = (int)new_numbertor / new_denominator;
-	new_numbertor = new_numbertor -  new_whole_number;
-	new_numerator = negative^X.negative ? -new_numerator : numerator;
-	return fraction(new_numerator,new_denominator,new_whole_number);
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if( whole_number != 0 && X.whole_number != 0){
+		int new_whole_number = (int)new_numerator / new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator,new_denominator,new_whole_number);
+	}
+	else{
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator ,new_denominator);
+	}
 }	
 
+
+// for fraction<int>, fraction reduction will be done.
+template<>
+fraction<int> fraction<int> :: operator/(const fraction &X) const{
+	int new_numerator = (numerator + whole_number*denominator) * X.denominator;
+	int new_denominator = denominator * (X.numerator + X.whole_number*X.denominator);
+	// the new denominator might be 0;
+	assert(new_denominator!=0);
+
+	// if inputs are both mixed fraction, then return mixed fraction.
+	if( whole_number != 0 && X.whole_number != 0){
+		int new_whole_number = (int)new_numerator / new_denominator;
+		new_numerator = fmod(new_numerator , new_denominator);
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator/greatest_common_divisor ,new_denominator/greatest_common_divisor, new_whole_number);
+	}
+	
+	else{	
+		// fraction reduction
+		int greatest_common_divisor = GCD(new_numerator,new_denominator);
+		new_numerator = negative^X.negative ? -new_numerator : new_numerator;
+		return fraction(new_numerator /greatest_common_divisor ,new_denominator/greatest_common_divisor);
+	}
+}	
 
 template<class T>
 bool fraction<T> :: operator==(const fraction &X) const{
@@ -101,7 +263,7 @@ bool fraction<T> :: operator!=(const fraction &X) const{
 
 
 template<class T>
-bool fraction<T> :: operator>(const fraction &X) const;{
+bool fraction<T> :: operator>(const fraction &X) const{
 	if( negative && !X.negative) return false;
 	if( !negative && X.negative) return true;
 	double value1 = (double)numerator/denominator + (double)whole_number;
@@ -111,7 +273,7 @@ bool fraction<T> :: operator>(const fraction &X) const;{
 
 
 template<class T>
-bool fraction<T> :: operator<(const fraction &X) const;{
+bool fraction<T> :: operator<(const fraction &X) const{
 	if( negative && !X.negative) return true;
 	if( !negative && X.negative) return false;
 	double value1 = (double)numerator/denominator + (double)whole_number;
@@ -124,12 +286,26 @@ bool fraction<T> :: operator<(const fraction &X) const;{
 template<class T>
 void fraction<T> :: display() const{
 	if( whole_number == 0){
-		if( negative) cout<<"-("<<numerator<<'\'<<denominator<<')'<<endl;
-		else cout<<numerator<<'\'<<denominator<<endl;
+		if( negative) cout<<"-("<<numerator<<'/'<<denominator<<')'<<endl;
+		else cout<<numerator<<'/'<<denominator<<endl;
 	}
 	else{
-		if( negative) cout<<'-'<<whole_number<<'('<<numerator<<'\'<<denominator<<')'<<endl;
-		else cout<<whole_number<<'('<<numerator<<'\'<<denominator<<')'<<endl;
+		if( negative) cout<<'-'<<whole_number<<'('<<numerator<<'/'<<denominator<<')'<<endl;
+		else cout<<whole_number<<'('<<numerator<<'/'<<denominator<<')'<<endl;
 	}
 }
+
+
+
+
+
+int GCD(int u, int v){
+	while( v != 0){
+		int r = u % v;
+		u = v;
+		v = r;
+	}
+	return u;
+}
+
 
